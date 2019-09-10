6b7d81f8e05d70357c2ade06beacf98178e8c975
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-09-10 16:56:38 -0700

    Heroku push

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,9 @@
+// module.exports = {
+//     mongoURI: "mongodb+srv://testing:triptipstesting@cluster0-ar1ls.mongodb.net/test?retryWrites=true&w=majority",
+//     secretOrKey: "secret"
+// };
+
 module.exports = {
-    mongoURI: "mongodb+srv://testing:triptipstesting@cluster0-ar1ls.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-};
+    mongoURI: "mongodb://<newtriptips>:<testing24>@ds149040.mlab.com:49040/heroku_x1p6d80d",
+    sexretOrKey: "secret"
+}
